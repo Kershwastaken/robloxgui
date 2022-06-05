e345cbec10a677543b76d4c4f387b5044a35e12e
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 10:58:28 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -12,22 +12,8 @@ local ESP = Render.CreateOptionsButton({
     end,
     ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
     ["Default"] = true, -- enabled on startup (optional)
-})
-thanosdagamer — Today at 10:52 AM
-```local ESP = Render.CreateOptionsButton({
-    ["Name"] = "ESP", -- name of object
-    ["Function"] = function(callback) -- function that is called when toggled
-        if callback then
-            print("enabled")
-        else
-            print("disabled")
-        end
-    end,
-    ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
-    ["Default"] = true, -- enabled on startup (optional)
     ["ExtraText"] = function() return " Placeholder" end -- text that goes next to the button in Text GUI (optional)
-})
-ESP.CreateSlider({
+})ESP.CreateSlider({
     ["Name"] = "Slider1", -- name of object
     ["Min"] = 0,
     ["Max"] = 100,
@@ -36,15 +22,13 @@ ESP.CreateSlider({
     end,
     ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
     ["Default"] = 50 -- default value (optional)
-})
-ESP.CreateColorSlider({
+})ESP.CreateColorSlider({
     ["Name"] = "Slider1", -- name of object
     ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
     ["Function"] = function(val) -- function that is called when the slider changes
         print(val)
     end
-})
-ESP.CreateToggle({
+})ESP.CreateToggle({
     ["Name"] = "Toggle1", -- name of object
     ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
     ["Function"] = function(callback) -- function that is called when toggled
@@ -55,15 +39,13 @@ ESP.CreateToggle({
         end
     end,
     ["Default"] = true -- Value upon launch (optional)
-})
-ESP.CreateDropdown({
+})ESP.CreateDropdown({
 	["Name"] = "Mode", -- name of object
 	["List"] = {"apple", "banana"}, -- list of strings to choose from
 	["Function"] = function(val) -- function that is called when you choose a string
             print(val)
 	end
-})
-ESP.CreateTextList({
+})ESP.CreateTextList({
 	["Name"] = "TextList1", -- name of the object
 	["TempText"] = "Placeholder", -- text that is shown
 	["AddFunction"] = function(val) -- called when a entry is added (optional)
@@ -75,8 +57,7 @@ ESP.CreateTextList({
    	["CustomFunction"] = function(obj, val) -- called when the entry is finished rendering so you can add custom rendering (optional)
    	    print("Finished Rendering : ", obj, " : ", val)
    	end
-})
-local overlay = GuiLibrary.CreateCustomWindow({
+})local overlay = GuiLibrary.CreateCustomWindow({
 	["Name"] = "Overlay", 
 	["Icon"] = "vape/assets/TargetIcon1.png", -- currently you have to use vape assets for icons, this may change in the future
 	["IconSize"] = 16 -- size in width to not look ugly
@@ -84,9 +65,9 @@ local overlay = GuiLibrary.CreateCustomWindow({
 overlay["Bypass"] = true -- making the window position save properly
 --overlay.SetVisible(true) -- not a toggle, pass in true or false
 -- you can put objects in
--- overlay.GetCustomChildren()
- -- button name + OptionsButton
+-- overlay.GetCustomChildren() -- button name + OptionsButton
  -- button can be a var, else you use GuiLibrary["ObjectsThatCanBeSaved"]["ESPOptionsButton"]["Api"] to get objects outside your code.
 button["ToggleButton"](false) -- toggles button
 button["SetKeybind"]("KeyCode in text, example LeftShift") -- sets keybind
 GuiLibrary["ObjectsThatCanBeSaved"]["ESPOptionsButton"]["Object"] -- gets the object of the button for customization and what not
+shared.VapeManualLoad = true
