e03c357b149a848135612de9343009dd671ff11d
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 17:19:33 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,10 +1,43 @@
-local walkspeed = true
-if walkspeed == true then
-game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = "54"
+repeat wait() until game:IsLoaded() == true
+if setclipboard then setclipboard("https://discord.gg/VjzccAHzR4")  end
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = Library:MakeWindow({Name = "ThanosDaGamer's Hub", HidePremium = false, SaveConfig = false, ConfigFolder = "ThanosDaGamerHub"})
 
-end
-shared.guiprivate = true
-if shared.guiprivate  == true then
-game:GetService("Workspace").Gravity = "1"
+Library:MakeNotification({
+    Name = "Welcome!",
+    Content = "And by the way, you should join our discord!\nInvite: VjzccAHzR4",
+    Image = "rbxassetid://4483345998",
+    Time = 10,
+})
 
-end
+local movement = Window:MakeTab({
+    Name = "Movmeent",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false,
+})
+
+movement:AddSlider({
+    Name = "WalkSpeed",
+    Min = 16,
+    Max = 54,
+    Default = 5,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "Speed",
+    Callback = function(v)
+        game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = v
+    end    
+})
+movement:AddSlider({
+    Name = "Gravity",
+    Min = 0,
+    Max = 197,
+    Default = 5,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "GravityToSet",
+    Callback = function(v)
+        game:GetService("Workspace").Gravity = v
+    end    
+})
+Library:Init()
