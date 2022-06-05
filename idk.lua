242720f6dcb138da5bcbb6f9cad722eef05b57e4
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 10:43:40 -0400

    Create idk.lua

---
+++ idk.lua
@@ -0,0 +1,3 @@
+shared.VapeIndependent = true
+shared.CustomSaveVape = "name of file to save"
+local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
