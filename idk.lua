b7986bc3f70aab027d3facd631b45f54d70710af
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 10:48:56 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,3 +1,14 @@
 shared.VapeIndependent = true
 shared.CustomSaveVape = "name of file to save"
 local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+local ESP = Render.CreateOptionsButton({
+    ["Name"] = "ESP", -- name of object
+    ["Function"] = function(callback) -- function that is called when toggled
+        if callback then
+            print("enabled")
+        else
+            print("disabled")
+        end
+    end,
+    ["HoverText"] = "Placeholder", -- text that will show up after hovering over the button (optional)
+    ["Default"] = true, -- enabled on startup (optional)
