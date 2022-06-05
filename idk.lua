ba1d0fb6816e2278b39cfd435ad638b5c950613d
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 19:52:39 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -48,48 +48,6 @@ local combat = Window:MakeTab({
     Name = "Combat",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
-})
-combat:AddButton({
-    Name = "aimbot",
-    Callback = function()
-              --> variables
-local UIS = game:GetService("UserInputService")
-local camera = game.Workspace.CurrentCamera
---> getting the closest player
-function getClosest()
-local closestPlayer = nil
-local closesDist = math.huge
-for i,v in pairs(game.Players:GetPlayers()) do
-if v ~= game.Players.LocalPlayer then
-local Dist = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).magnitude
-if Dist < closesDist then
-closesDist = Dist
-closestPlayer = v
-end
-end
-end
-return closestPlayer
-end
-
---> starting the aimbot
-_G.aim = false
-UIS.InputBegan:Connect(function(inp)
-    if inp.UserInputType == Enum.UserInputType.MouseButton2 then
-    _G.aim = true
-    while wait() do
-        camera.CFrame = CFrame.new(camera.CFrame.Position,getClosest().Character.Head.Position)
-        if _G.aim == false then return end
-    end
-    end
-end)
---> ending the aimbot
-UIS.InputEnded:Connect(function(inp)
-    if inp.UserInputType == Enum.UserInputType.MouseButton2 then
-    _G.aim = false
-    end
-end)
-      end    
-})
-})
+})  
 Library:Init()
 
