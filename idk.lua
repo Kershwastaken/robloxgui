dd8ef5693013b850e31fe50916cb1d780d01d4eb
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 15:30:10 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -7,10 +7,3 @@ shared.guiprivate = true
 if shared.guiprivate  == true then
 game:GetService("Workspace").Gravity = "1"
 end
-local humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid
-humanoid.Running:Connect(function(speed)
-    humanoid.WalkSpeed = 0
-    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame + game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector 
-    wait(.25)
-    humanoid.WalkSpeed = 16
-end)
