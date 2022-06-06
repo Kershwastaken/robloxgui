05c2629243882207c0d540384ba8abba2e4d5fd5
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 20:08:11 -0400

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,71 +1,68 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "betagui", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Window:MakeTab({
-	Name = "movment",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+repeat wait() until game:IsLoaded() == true
+if setclipboard then setclipboard("https://discord.gg/VjzccAHzR4")  end
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = Library:MakeWindow({Name = "ThanosDaGamer's Hub", HidePremium = false, SaveConfig = false, ConfigFolder = "ThanosDaGamerHub"})
+
+Library:MakeNotification({
+    Name = "Welcome!",
+    Content = "And by the way, you should join our discord!\nInvite: VjzccAHzR4",
+    Image = "rbxassetid://4483345998",
+    Time = 10,
 })
 
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
-OrionLib:MakeNotification({
-	Name = "betagui",
-	Content = "thanks for using my script",
-	Image = "rbxassetid://4483345998",
-	Time = 5
+local movement = Window:MakeTab({
+    Name = "Movement",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false,
 })
 
---[[
-Title = <string> - The title of the notification.
-Content = <string> - The content of the notification.
-Image = <string> - The icon of the notification.
-Time = <number> - The duration of the notfication.
-]]
-Tab:AddButton({
-	Name = "speed",
-	Callback = function()
-      		local walkspeed = true
-if walkspeed == true then
-game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = "54"
-  	end    
-})
-
---[[
-Name = <gravity 0> - The name of the button.
-Callback = <function> - The function of the button.
-  Tab:AddButton({
-	Name = "Button!",
-	Callback = function()
-      		shared.guiprivate = true
-if shared.guiprivate  == true then
-game:GetService("Workspace").Gravity = "1"
-
-  	end    
-})
-
---[[
-Name = <string> - The name of the button.
-Callback = <function> - The function of the button.
-]]
 local combat = Window:MakeTab({
     Name = "Combat",
     Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
+    PremiumOnly = false,
 })
-combant:AddButton({
+
+movement:AddSlider({
+    Name = "WalkSpeed",
+    Min = 16,
+    Max = 54,
+    Default = 5,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "Speed",
+    Callback = function(v)
+        game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = v
+    end    
+})
+movement:AddSlider({
+    Name = "Gravity",
+    Min = 0,
+    Max = 197,
+    Default = 5,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "GravityToSet",
+    Callback = function(v)
+        game:GetService("Workspace").Gravity = v
+    end
+Tab:AddButton({
+    Name = "Button!",
+    Callback = function()
+              print("button pressed")
+      end
+Tab:AddButton({
     Name = "aimbot",
     Callback = function()
               _G.AimbotInput = "RightClick" -- // RightClick, LeftClick, Q, etc...
-_G.AimbotEasing = 0.2 -- // Stage of Linear Easing to target when enabled
+_G.AimbotEasing = .1 -- // Stage of Linear Easing to target when enabled
 _G.TeamCheck = true -- // Checks the team of the target to make sure they're not on your team
 
 ---------------------------------------------------------------------------------------------------
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-aimbot/main/script.lua"))())
-      end    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-aimbot/main/script.lua"))()
+      end 
+
 })
-]]
-OrionLib:Init()
+})
+})
+Library:Init()
