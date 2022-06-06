8db97f2116e0762786e41499016c6d4f39d6fd1a
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-06-05 20:17:44 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -39,7 +39,7 @@ movement:AddSlider({
     Callback = function(v)
         game:GetService("Workspace").Gravity = v
     end
-local movement = Window:MakeTab({
+local comabt = Window:MakeTab({
     Name = "combat",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false,
