8e2cd14e5d4bf4bc7e6e25851eb1d620bc97fc88
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-07-04 16:24:23 -0400

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -26,12 +26,15 @@ combat:NewButton("aimbot", "rightclick to lock screen on people who arent on tea
         _G.TeamCheck = true
         loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-aimbot/main/script.lua"))()
 end)
+combat:NewButton("silent aim", "auto aim no screen lock", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
+end)
 
 render:NewButton("esp", "easily see players", function()
     _G.FriendColor = Color3.fromRGB(0, 0, 255)
 	_G.EnemyColor = Color3.fromRGB(255, 0, 0)
 	_G.UseTeamColor = false
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Thanosdagamer/silentaimscript/main/silentaim.lua"))()
 end)
 
 credits:NewLabel("me (thanosdagamer#6469)")
