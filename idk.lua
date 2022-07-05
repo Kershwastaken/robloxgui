4be95adcd011a6c9dfe6398017ec561e2c29bb4d
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-07-05 11:24:21 -0400

    made it look cleaner

--- idk.lua
+++ idk.lua
@@ -4,42 +4,49 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 
 local Window = Library.CreateLib("thanos hub", "Ocean")
 
-local main = Window:NewTab("main")
 
-local movement = main:NewSection("movement")
+local movement = Window:NewTab("movement")
 
-local combat = main:NewSection("combat")
+local combat = Window:NewTab("combat")
 
-local render = main:NewSection("render")
+local render = Window:NewTab("render")
 
-local credits = main:NewSection("credits")
+local credits = Window:NewTab("credits")
 
-movement:NewSlider("speed", "changes speed", 100, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+local movementsection = movement:NewSection("movement")
+
+local combatsection = combat:NewSection("combat")
+
+local rendersection = render:NewSection("render")
+
+local creditssection = credits:NewSection("credits")
+
+movementsection:NewSlider("speed", "changes speed", 100, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-movement:NewSlider("gravity", "change ur gravity",500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+movementsection:NewSlider("gravity", "change ur gravity",500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game:GetService("Workspace").Gravity = s
 end)
 
-combat:NewButton("aimbot", "rightclick to lock screen on people who arent on team", function()
+combatsection:NewButton("aimbot", "rightclick to lock screen on people who arent on team", function()
     G.AimbotInput = "RightClick"
         _G.AimbotEasing = 0.000000000000001
         _G.TeamCheck = true
         loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-aimbot/main/script.lua"))()
 end)
-combat:NewButton("silent aim", "auto aim no screen lock", function()
+combatsection:NewButton("silent aim", "auto aim no screen lock", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Thanosdagamer/silentaimscript/main/silentaim.lua"))()
 end)
 
-render:NewButton("esp", "easily see players", function()
+rendersection:NewButton("esp", "easily see players", function()
     _G.FriendColor = Color3.fromRGB(0, 0, 255)
 	_G.EnemyColor = Color3.fromRGB(255, 0, 0)
 	_G.UseTeamColor = false
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
 end)
 
-credits:NewLabel("me (thanosdagamer#6469)")
+creditssection:NewLabel("me (thanosdagamer#6469)")
 
-credits:NewLabel("someone i wont mention cuz they asked")
+creditssection:NewLabel("someone i wont mention cuz they asked")
 
